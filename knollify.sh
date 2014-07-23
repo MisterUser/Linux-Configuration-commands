f2da5feb5922ef238d1f45c24a94d33d9d56fe7d
MisterUser <gregknoll@gmail.com>
2014-07-23 14:44:07 +0200

    Create knollify.sh

---
+++ knollify.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+Linux-Configuration-commands
+============================
+
+Commands to customize a new installation/system
+
+#always have syntax highlighting on in vim
+echo "syntax on" > ~/.vimrc
+
+#make vimdiff default git difftool
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global alias.dt difftool
+git config --global merge.tool vimdiff
+
+
+#---Aliases---#
+#monitor system log with syntax highlighting
+alias syshow='sudo tail -n 50 -F /var/log/syslog | source-highlight --out-format=esc -o STDOUT -s log'
+alias crep='GREP_COLOR="1;35" grep --color'
+alias pout='ps aux | grep -v "grep" | GREP_COLOR="1;35" grep --color "$@"'
