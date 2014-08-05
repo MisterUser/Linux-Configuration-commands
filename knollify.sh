50a3f8764698ba8e372fd967afdcf79ec128a557
MisterUser <gregknoll@gmail.com>
2014-08-05 18:38:46 +0200

    Update knollify.sh

--- knollify.sh
+++ knollify.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-Linux-Configuration-commands
-============================
+#Linux-Configuration-commands#
+#============================#
 
-Commands to customize a new installation/system
+#Commands to customize a new installation/system
 
 #always have syntax highlighting on in vim
 echo "syntax on" > ~/.vimrc
@@ -20,3 +20,10 @@ git config --global merge.tool vimdiff
 sed -i '$a alias syshow=\x27sudo tail -n 50 -F /var/log/syslog | source-highlight --out-format=esc -o STDOUT -s log\x27' ~/.bash_aliases
 sed -i '$a alias crep=\x27GREP_COLOR="1;35" grep --color\x27' ~/.bash_aliases
 sed -i '$a alias pout=\x27ps aux | grep -v "grep" | GREP_COLOR="1;35" grep --color "$@"\x27' ~/.bash_aliases
+sed -i '$a alias la=\x27ls -a\x27' ~/.bash_aliases
+sed -i '$a alias ll=\x27ls -l -a\x27' ~/.bash_aliases
+
+
+
+#----Command Prompt Color----#
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\n\n\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\n\[\033[01;32m\]\$\[\033[00m\]'
